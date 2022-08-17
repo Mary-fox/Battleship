e60db2d9541609d6f1c24cc3bc699fafe25e5c70
Mary <89971822+Mary-fox@users.noreply.github.com>
2022-08-17 16:09:17 +0300

    battleship [feat] -add object

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-function doIt(param) {
-    param = 2;
-   }
-   let test = 1;
-   doIt(test);
-   console.log(test);t
+let view = {
+    displayMessage: function(msg) {
+        let messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
+    },
+    displayHit: function(location) {
+        let cell = document.getElementById(location);
+        cell.setAttribute("class", "hit");
+    },
+    displayMiss: function(location){
+        let cell = document.getElementById(location);
+        cell.setAttribute("class", "miss");
+    }
+}
